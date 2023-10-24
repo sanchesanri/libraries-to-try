a1958620ede8aa374f4e20108c36e71cb58fcb2b
AnriSanches <89216270+sanchesanri@users.noreply.github.com>
2023-10-24 19:17:52 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,25 @@
+
+// import { nanoid } from 'nanoid'
+// import iro from '@jaames/iro';
+
+// const btnStop = document.querySelector('.stop-picker');
+// btnStop.addEventListener('click', onColorChangeStop)
+
+// const moduleId= nanoid(10) 
+// const colorPicker = new iro.ColorPicker('#picker',{
+//     width: 220,
+//     layoutDirection: 'horizontal',
+//     borderWidth: 2,
+//     borderColor: "#77e6b6",
+//   });
+
+//   colorPicker.on('color:change', onColorChange);
+
+//   function onColorChange (color) {
+//     document.body.style.background = color.hexString
+//   }
+
+//   function onColorChangeStop () {
+//     colorPicker.off('color:change', onColorChange);
+//   }
+
